8e90e0c64fc1737a5fa7782f9e673fdf105d626d
antieatingactivist <antieatingactivist@gmail.com>
2022-09-10 17:13:42 -0700

    added dynamic sizing of display

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -12,5 +12,22 @@ export class DisplayComponent implements OnInit {
   @Input() isOn: boolean = true;
   ngOnInit(): void {
   }
+  getStyle(): object {
+    return {
+      fontSize: this.determineSize(),  
+    }
+  }
+  private determineSize(): string {
+    const x = this.currentDisplay.length;
+    switch (true) {
+      case (x > 26): return '14px';
+      case (x > 21): return '16px';
+      case (x > 14): return '20px';
+      case (x > 10): return '30px';
+      case (x > 8): return '40px';
+      default: return '50px';
+    }
+    
+  }
 
 }
